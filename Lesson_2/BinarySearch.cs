0c0b0d52513100a30b6c4b8098d705a0fc6d361b
Indeezzz <indeeezzz@mail.ru>
2021-10-04 03:25:41 +0300

    Домашняя Работа №2

---
+++ Lesson_2/BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Lesson_2
+{
+    class BinarySearch
+    {
+        public static int[] mass = new int[] { 1, 2, 3, 4, 5 };
+        public static int BiSearch(int[] inputArray, int searchValue)
+        {
+
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {                  
+                    Node.Print("Искомый элемент под индексом: " + mid + "\n");
+                    Node.Print("Аимптотическая сложность: O(log n)");
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }           
+            return -1;
+        }
+    }
+}
---
+++ Lesson_2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lesson_2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            TestNode.TestListNode();
+            BinarySearch.BiSearch(BinarySearch.mass, 4);
+        }
+    }
+}
